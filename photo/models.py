d4e799e98b42a7eb40fdd25b1f1cde7fda8fb4cd
Daniel-darnell <darnelldanny1997@gmail.com>
2020-11-18 11:33:13 +0300

    Created a Photos, Location and Category class models

--- photo/models.py
+++ photo/models.py
@@ -2,15 +2,75 @@ from django.db import models
 
 # Create your models here.
 class Photo(models.Model):
-    title = models.CharField(max_length=30)
-    post = models.TextField()
-    category = models.CharField(max_length=30)
-    editor = models.ForeignKey(Editor)
+    title = models.CharField(max_length=120)
+    location = modelsfrom django.db import models
 
-class Editor(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
+class Photos(models.Model):
+    name =models.CharField(max_length = 30)
+    photo_description = models.TextField()
+    photo_location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
+    photo_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
+    photo = models.ImageField(upload_to = 'images/', default='image.jpg')
+
+    
+    def save_photo(self):
+        self.save()
+
+    def delete_photo(self):
+        self.save()
+
+    @classmethod
+    def search_by_photo_category(cls,search_term):
+        photo = cls.objects.filter(name__icontains = search_term)
+        return photo
+
+    def update_image(self, Name=None, category=None):
+        self.name = Name if Name else self.Name
+        self.photo_category = category if category else self.photo_category 
+        self.save()
+
+    
 
     def __str__(self):
-        return self.first_name
+        return self.name
+    
+    
+class Location(models.Model):
+    location_name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.location_name
+
+    def save_location_name(self):
+        self.save()
+
+    def delete_location_name(self):
+        self.delete()
+
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(location_name=value)
+        
+
+
+
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length = 30)
+
+    def save_category_name(self):
+        self.save()
+
+    def delete_category_name(self):
+        self.delete()
+
+    def __str__(self):
+        return self.category_name
+
+
+    
+
+    
+
