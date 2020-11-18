ae903a4a869199eaa9602303d08a2d50155c334d
Daniel-darnell <darnelldanny1997@gmail.com>
2020-11-18 06:52:15 +0300

    Created my Editor model and made migrations

--- photo/models.py
+++ photo/models.py
@@ -5,3 +5,8 @@ class Photo(models.Model):
     name = models.CharField(max_length=30)
     location = models.CharField(max_length=30)
     category = models.CharField(max_length=30)
+
+class Editor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
